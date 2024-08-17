05b143c52cd947314fe1cf22caa0b37ca8bfa0b5
taanphat02 <taanphaat02@gmail.com>
2024-08-18 02:40:58 +0700

    react native to acquaint something

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.text1}>Count = {count}</Text>
+        <Button title="increse" onPress={() => setCount(count + 1)} />
+      </View>
+
+      <View>
+        <Text style={styles.text1}>My name is: {name}</Text>
+        <TextInput
+          multiline={true}
+          style={styles.input}
+          onChangeTexgit status
+          t={(value) => setName(value)}
+        />
+      </View>
+
+      <View>
+        <Text style={styles.text1}>I am {age} year old</Text>
+        <TextInput
+          style={styles.input}
+          maxLength={2}
+          keyboardType="numeric"
+          onChangeText={(value) => setAge(+value)}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +40,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+  text1: {
+    color: "red",
+    fontSize: 20,
+    padding: 10,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 8,
   },
 });
