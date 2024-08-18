33604ac2631e33f29aa77adfc8f6121a376147a8
taanphat02 <taanphaat02@gmail.com>
2024-08-19 00:06:56 +0700

    get used to flexbox(basic) with reactNative

--- App.tsx
+++ App.tsx
@@ -1,38 +1,92 @@
 import { useState } from "react";
 import {
+  Alert,
   Button,
+  FlatList,
+  Keyboard,
+  Pressable,
   ScrollView,
   StyleSheet,
   Text,
   TextInput,
+  TouchableWithoutFeedback,
   View,
 } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+interface ITodo {
+  id: number;
+  name: string;
+}
 
 export default function App() {
-  const [students, setStudents] = useState([
-    { id: 1, name: "Nguyen Van A", age: 18 },
-    { id: 2, name: "Nguyen Van B", age: 20 },
-    { id: 3, name: "Nguyen Van C", age: 42 },
-    { id: 4, name: "Nguyen Van AB", age: 24 },
-    { id: 5, name: "Nguyen Van AC", age: 14 },
-    { id: 7, name: "Nguyen Van ABC", age: 12 },
-    { id: 8, name: "Nguyen Van BAC", age: 11 },
-    { id: 9, name: "Nguyen Van CBA", age: 20 },
-  ]);
+  const [todo, setTodo] = useState("");
+  const [listTodo, setListTodo] = useState<ITodo[]>([]);
+
+  function randomInteger(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const handleAddTodo = () => {
+    if (!todo) {
+      Alert.alert("Message", "Please enter todo");
+      return;
+    }
+    setListTodo([...listTodo, { id: randomInteger(2, 50000), name: todo }]);
+    setTodo("");
+  };
+
+  const handleDeleteItem = (id: number, name: string) => {
+    Alert.alert("Delete", `Do you want to delete ${name}`, [
+      {
+        text: "Cancle",
+        onPress: () => {},
+        style: "cancel",
+      },
+      {
+        text: "OK",
+        onPress: () => {
+          const newTodos = listTodo.filter((item) => item.id !== id);
+          setListTodo(newTodos);
+        },
+      },
+    ]);
+  };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading1}>ARRAY STUDENT</Text>
-      <ScrollView>
-        {students.map((item) => {
-          return (
-            <View>
-              <Text style={styles.layoutItem}>{item.name}</Text>
-            </View>
-          );
-        })}
-      </ScrollView>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Text style={styles.heading1}>Todo List</Text>
+
+        <TextInput
+          value={todo}
+          style={styles.inputText}
+          onChangeText={(value) => setTodo(value)}
+        />
+
+        <View style={styles.button}>
+          <Button title="Add todo" onPress={handleAddTodo} />
+        </View>
+
+        <View style={styles.list}>
+          <FlatList
+            keyExtractor={(item) => item.id + ""}
+            data={listTodo}
+            renderItem={({ item }) => {
+              return (
+                <Pressable onPress={() => handleDeleteItem(item.id, item.name)}>
+                  <View style={styles.group}>
+                    <Text>{item.name} </Text>
+                    <AntDesign name="closecircle" size={24} color="black" />
+                  </View>
+                </Pressable>
+              );
+            }}
+          />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+    // <FlexBox />
   );
 }
 
@@ -40,20 +94,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingHorizontal: 40,
-    paddingTop: 50,
-    paddingBottom: 50,
+    paddingHorizontal: 10,
+    paddingTop: 20,
   },
   heading1: {
     fontSize: 25,
-    color: "green",
     textAlign: "center",
-    paddingBottom: 10,
+    backgroundColor: "orange",
+    padding: 10,
   },
-  layoutItem: {
-    padding: 35,
-    backgroundColor: "lightsteelblue",
+  inputText: {
+    borderWidth: 0.5,
+    padding: 10,
+    margin: 20,
+    fontSize: 18,
+  },
+  button: {
+    marginHorizontal: 100,
     marginBottom: 15,
-    borderRadius: 5,
+  },
+  group: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderWidth: 0.8,
+    borderStyle: "dashed",
+    padding: 15,
+    marginBottom: 10,
+  },
+  list: {
+    flex: 1,
   },
 });
